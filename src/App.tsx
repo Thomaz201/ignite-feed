71bc36b71f335317495ccaad5e8d013d6d922120
Lucas Thomaz <lucasthomazr@hotmail.com>
2022-08-28 19:27:54 -0300

    Sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { Header } from './components/Header'
-
-import './global.css'
+import { Sidebar } from './components/Sidebar'
+import styles from './styles/App.module.css'
+import './styles/global.css'
 
 export function App() {
 
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+        </main>
+      </div>
     </>
   )
 }
